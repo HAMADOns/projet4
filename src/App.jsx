1a84ee3d204b161b8eeeba6d2146b2540bb5dbac
Ons HAMAD <onshamadkl@gmail.com>
2024-03-27 22:10:46 +0100

    coupe

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,26 @@ const principalContainer = (
     </p>
   </div>
 );
+const secondContainer = (
+  <div
+    className="container"
+    style={{ color: "#cac6c6", backgroundColor: "#1f2a36" }}
+  >
+    <img src="../public/coupe.png" alt="" className="image" />
+    MERCI
+    <p className="text" style={{ color: "#a5a1a1" }}>
+      Votre feedback est crucial pour nous aider à améliorer nos services et à
+      mieux répondre à vos besoins à l'avenir
+    </p>
+  </div>
+);
 function test() {}
 function App() {
   return (
     <>
       <div>Projet 4</div>
       {principalContainer}
+      {secondContainer}
     </>
   );
 }
