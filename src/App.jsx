458a699c784a722d4d6eb9162a63dc156f05906b
Ons HAMAD <onshamadkl@gmail.com>
2024-03-27 23:58:07 +0100

    onClick

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,6 @@ import * as React from "react";
 import "./App.css";
 import "./styles.css";
 
-const principalContainer = (
-  <div
-    className="container"
-    style={{ color: "#cac6c6", backgroundColor: "#1f2a36" }}
-  >
-    <img src="../public/etoile5.png" alt="" className="image" />
-    Votez pour moi
-    <p className="text" style={{ color: "#a5a1a1" }}>
-      Nous aimerions connaître votre opinion sur votre récente expérience avec
-      notre service. Veuillez prendre quelques instants pour nous attribuer une
-      note de satisfaction de 1 à 5
-    </p>
-    <div>
-      <button
-        type="submit"
-        style={{
-          color: "white",
-          fontWeight: "bold",
-          backgroundColor: "#f59f01",
-          textAlign: "center",
-        }}
-      >
-        Voter
-      </button>
-    </div>
-  </div>
-);
 const secondContainer = (
   <div
     className="container"
@@ -42,14 +15,51 @@ const secondContainer = (
     </p>
   </div>
 );
-function Vote() {
-  return <>{principalContainer}</>;
+function vote() {
+  console.log({ secondContainer });
+  return (
+    <div
+      className="container"
+      style={{ color: "#cac6c6", backgroundColor: "#1f2a36" }}
+    >
+      <img src="../public/coupe.png" alt="" className="image" />
+      MERCI
+      <p className="text" style={{ color: "#a5a1a1" }}>
+        Votre feedback est crucial pour nous aider à améliorer nos services et à
+        mieux répondre à vos besoins à l'avenir
+      </p>
+    </div>
+  );
 }
 function App() {
   return (
     <>
       <div>Projet 4</div>
-      <Vote />
+      <div
+        className="container"
+        style={{ color: "#cac6c6", backgroundColor: "#1f2a36" }}
+      >
+        <img src="../public/etoile5.png" alt="" className="image" />
+        Notez notre service
+        <p className="text" style={{ color: "#a5a1a1" }}>
+          Nous aimerions connaître votre opinion sur votre récente expérience
+          avec notre service. Veuillez prendre quelques instants pour nous
+          attribuer une note de satisfaction de 1 à 5
+        </p>
+        <div>
+          <button
+            style={{
+              color: "white",
+              fontWeight: "bold",
+              backgroundColor: "#f59f01",
+              textAlign: "center",
+            }}
+            onClick={vote}
+          >
+            Noter
+          </button>
+        </div>
+      </div>
     </>
   );
 }
