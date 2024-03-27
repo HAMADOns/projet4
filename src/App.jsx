9173634686e86c034cb18e030410b54c38647045
Ons HAMAD <onshamadkl@gmail.com>
2024-03-27 21:24:32 +0100

    container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import * as React from "react";
 import "./App.css";
+import "./styles.css";
+
+const principalContainer = (
+  <div
+    className="container"
+    style={{ color: "white", backgroundColor: "#0a0a27" }}
+  >
+    Notez notre service
+  </div>
+);
 function test() {}
 function App() {
   return (
     <>
       <div>Projet 4</div>
+      {principalContainer}
     </>
   );
 }
